28c26e612ad5b445a15ec04f17db34f27700dd10
yaron <yaron57@gmail.com>
2016-02-29 22:26:26 +0300

    Version 1.1
    Change-Id: I1380bbbf5758b4b60f5f2c2c59fd817626efd817

--- HeaderTabs.php
+++ HeaderTabs.php
@@ -33,7 +33,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'Header Tabs',
 	'descriptionmsg' => 'headertabs-desc',
-	'version' => '1.1-alpha',
+	'version' => '1.1',
 	'author' => array( '[http://www.sergeychernyshev.com Sergey Chernyshev]', 'Yaron Koren', '[http://olivierbeaton.com Olivier Finlay Beaton]' ),
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Header_Tabs'
 );
